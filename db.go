697062069f28651daff2ea3cdfb367e13abe4963
p0tr3c <p0tr3c@protonmail.com>
2023-06-02 09:26:32 +0000

    feat: allow user filter

--- db.go
+++ db.go
@@ -33,9 +33,9 @@ func (p *product) createProduct(db *sql.DB) error {
 	}
 	return nil
 }
-func getProducts(db *sql.DB, start, count int) ([]product, error) {
+func getProducts(db *sql.DB, filter string, start, count int) ([]product, error) {
 	rows, err := db.Query(
-		"SELECT id, name,  price FROM products LIMIT $1 OFFSET $2",
+		"SELECT id, name,  price FROM products WHERE "+filter+" LIMIT $1 OFFSET $2",
 		count, start)
 	if err != nil {
 		return nil, err
